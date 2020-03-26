670ad5902293090586b7a790fdcbc430daa70a11
Brian Timar <timarbrian@gmail.com>
2020-03-26 13:53:50 -0700

    plotting token probs

--- tokens.py
+++ tokens.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
 
 """Statistics on the tokenized dataset."""
 
@@ -41,7 +43,44 @@ class TokenSet:
             raise ValueError
         return self._count        
 
-if __name__ == "__main__":
-    tokenpath = "data/gibbon_daf_tokens.txt"
+def write_token_stats(tokenpath, statsfile):
+        tokens = TokenSet(tokenpath)
+        sw = tokens.sorted_words
+        with open(statsfile, 'w') as f:
+            f.write(f"Token statistics from {tokenpath}\n")
+            f.write(f"Total token count: {len(tokens)}\n")
+            f.write("tokens sorted by count follow\n")
+            f.write("Word, count, probability\n")
+            f.write('-'*20 + "\n")
+
+            for word, count in sw:
+                f.write(f"{word}, {count}, {count/len(tokens):.6e}\n")
+
+def plot_token_probs(tokenpath):
     tokens = TokenSet(tokenpath)
     sw = tokens.sorted_words
+
+    min_ct = 5
+    counts_all = np.asarray([t[1] for t in sw])
+
+    counts = counts_all[counts_all>=min_ct]
+    probabilities = counts / len(tokens)
+    probabilities_all = counts_all / len(tokens)
+
+    H = -np.sum(probabilities_all * np.log2(probabilities_all))
+
+    fig, ax = plt.subplots()
+    ax.plot(np.log(probabilities)/np.log(10), '-o')
+    ax.set_xlabel("token index")
+    ax.set_ylabel("log10 p(word)")
+    ax.set_title(f"token probs, min count = {min_ct}. H={H:.3f} bits")
+    plt.savefig("token_probs.png")
+
+if __name__ == "__main__":
+    tokenpath = "data/gibbon_daf_tokens.txt"
+    statsfile = "data/token_stats.txt"
+
+    plot_token_probs(tokenpath)
+   
+
+
