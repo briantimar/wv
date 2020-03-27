9c3153376e29b6288674c46176868971c1cc12b8
Brian Timar <timarbrian@gmail.com>
2020-03-27 00:49:23 -0700

    added index iterator to tokenset

--- tokens.py
+++ tokens.py
@@ -12,6 +12,7 @@ class TokenSet:
         self._count = None
         self._multiset = None
         self._sorted_words = None
+        self._index_map = None
 
     def __iter__(self):
         with open(self.fname) as f:
@@ -26,22 +27,41 @@ class TokenSet:
         counts = self.multiset
         self._sorted_words = sorted(counts.items(), key=lambda t: -t[1])
 
+    def _build_index_map(self):
+        self._index_map = {word_tup[0] : i for i, word_tup in enumerate(self.sorted_words)}
+
     @property
     def multiset(self):
+        """a dictionary mapping words to frequencies."""
         if self._multiset is None:
             self._build_multiset()
         return self._multiset
     
     @property
     def sorted_words(self):
+        """A list of (word, count) pairs in descending order of count."""
         if self._sorted_words is None:
             self._sort_words()
         return self._sorted_words
+    
+    @property
+    def index_map(self):
+        """Dictionary mapping word strings to unique indices. These correspond to order when 
+        sorted by descending frequency."""
+        if self._index_map is None:
+            self._build_index_map()
+        return self._index_map
 
     def __len__(self):
         if self._count is None:
             raise ValueError
-        return self._count        
+        return self._count  
+
+    def indices(self):
+        """Iterator over indices of words in the training set. These are defined by frequency, namely as the
+        index of a word in the full list of words sorted by frequency."""
+        for word in iter(self):
+            yield self.index_map[word]
 
 def write_token_stats(tokenpath, statsfile):
         tokens = TokenSet(tokenpath)
@@ -80,7 +100,9 @@ if __name__ == "__main__":
     tokenpath = "data/gibbon_daf_tokens.txt"
     statsfile = "data/token_stats.txt"
 
-    plot_token_probs(tokenpath)
+    # plot_token_probs(tokenpath)
+
+
    
 
 
