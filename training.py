668f7e1890d7f69eb325a16829a8779bc730e76c
Brian Timar <timarbrian@gmail.com>
2020-03-27 17:43:22 -0700

    more logging

--- training.py
+++ training.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import json
 import time
 
-def sgd_train(model, context_iter, epochs, lr, wt_decay=0.0, logstep=100, verbose=False, logfile=None):
+def sgd_train(model, context_iter, epochs, lr, wt_decay=0.0, logstep=100, verbose=False, logfile=None, 
+                log_params = {}):
     """ Train the model via SGD on the given dataset. Gradients are *not* batched across different contexts.
         model: a SkipGramVW model.
         context_iter: iterator over input words and the corresponding context and noise tokens.
@@ -19,12 +20,11 @@ def sgd_train(model, context_iter, epochs, lr, wt_decay=0.0, logstep=100, verbos
 
     if logfile is None:
         logfile = datetime.now().strftime("sgd_log_%y_%m_%d__%H_%M_%S.json")
-
+    log_params.update({'loss': smoothed_losses, 'logstep': logstep, 
+                'lr': lr, 'wt_decay': wt_decay, 'epochs': epochs})
     def log():
-        _log = {'loss': smoothed_losses, 'logstep': logstep, 
-                'lr': lr, 'wt_decay': wt_decay, 'epochs': epochs}
         with open(logfile, 'w') as f:
-            json.dump(_log, f)
+            json.dump(log_params, f)
 
     for ep in range(epochs):
         t0 = time.time()
@@ -50,6 +50,14 @@ if __name__ == "__main__":
     from model import SkipGramWV
     tokenfile = "data/gibbon_daf_tokens.txt"
     ts = TokenSet(tokenfile)
-    ci = ContextIterator(ts, 5, num_noise=10)
-    model = SkipGramWV(ts.num_tokens, 10)
-    sgd_train(model, ci, 1, .1, logstep=1000, logfile="log.json", verbose=True)
+    context_radius = 5
+    num_noise = 20
+    vector_dim = 10
+    epochs = 1
+    lr = .1
+    logstep = 1000
+    ci = ContextIterator(ts, context_radius, num_noise=num_noise)
+    model = SkipGramWV(ts.num_tokens, vector_dim)
+    log_params = dict(num_noise=num_noise, vector_dim=vector_dim,context_radius=context_radius)
+    sgd_train(model, ci, epochs, lr, logstep=logstep, logfile="log.json", 
+                    verbose=True, log_params=log_params)
