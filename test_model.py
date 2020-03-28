5a33f61b2a469cccfc7ef60227307b848fc2d2ae
Brian Timar <timarbrian@gmail.com>
2020-03-27 19:55:36 -0700

    gradient tests

--- test_model.py
+++ test_model.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from model import SkipGramWV
+from model import SkipGramWV, sigma
 
 def diff(array1, array2):
     return np.sum((array1 - array2)**2)
@@ -56,6 +56,66 @@ class SkipGramWVTest(unittest.TestCase):
         noise_indices = [4, 12]
 
         self.model.do_sgd_update(input_index, context_indices, noise_indices, 0.01)
+
+
+class TestNumerics(unittest.TestCase):
+
+
+    def test_sigma(self):
+        inputs = [-np.log(2), 0, np.log(2)]
+        outputs = [1.0/3, .5, 2.0/3 ]
+        for i, o in zip(inputs, outputs):
+            self.assertAlmostEqual(o, sigma(i))
+
+    def do_grad_check(self, which, index, eps=1e-5):
+        """Perform a numerical gradient check with a small model
+            which = input, context, or noise
+                'input'-> checks grad of input vector. 
+                'context' -> checks grad wrt a context output vector
+                'noise' -> checks grad wrt a noise output vector
+            index = 0, 1. Pick a context or noise vector. If input, just set to zero."""
+        vocab_size = 5
+        vector_dim = 2
+        model = SkipGramWV(vocab_size, vector_dim)
+
+        input_index = 2
+        context_indices = [1, 3]
+        noise_indices = [0, 4]
+        ig, cg, ng = model._gradient_tensors(input_index, context_indices, noise_indices)
+        l1 = model.neg_loss(input_index, context_indices, noise_indices)
+
+        if which == "context":
+            model._output_vectors[context_indices[index]][0] += eps
+            agrad = cg[index,0]
+        elif which == "noise":
+            model._output_vectors[noise_indices[index]][0] += eps
+            agrad = ng[index, 0]
+        elif which == "input":
+            model._input_vectors[input_index][0] += eps
+            agrad = ig[0]
+        l2 = model.neg_loss(input_index, context_indices, noise_indices)
+        ngrad = (l2 - l1) / eps
+        return agrad, ngrad
+
+    def test_context_output_grads(self):
+        eps=1e-5
+        agrad, ngrad = self.do_grad_check("context", 0, eps=eps)
+        self.assertAlmostEqual(agrad, ngrad, delta=10*eps)
+        agrad, ngrad = self.do_grad_check("context", 1, eps=eps)
+        self.assertAlmostEqual(agrad, ngrad, delta=10*eps)
+
+
+    def test_noise_output_grads(self):
+        eps=1e-5
+        agrad, ngrad = self.do_grad_check("noise", 0, eps=eps)
+        self.assertAlmostEqual(agrad, ngrad, delta=10*eps)
+        agrad, ngrad = self.do_grad_check("noise", 1, eps=eps)
+        self.assertAlmostEqual(agrad, ngrad, delta=10*eps)
+
+    def test_input_grads(self):
+        eps=1e-5
+        agrad, ngrad = self.do_grad_check("input",0, eps=eps)
+        self.assertAlmostEqual(agrad, ngrad, delta=10*eps)
 
 if __name__ == "__main__":
     unittest.main()
